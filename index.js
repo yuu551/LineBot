5682147aa222698bad308d2291952a25b9098cf7
yuu24 <nakou36@gmail.com>
2020-09-12 22:15:34 +0900

    add api

--- index.js
+++ index.js
@@ -7,4 +7,7 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
+  .get('/g/', (req, res) => res.json({method: "こんにちは、getさん"}))
+  .post('/p/', (req, res) => res.json({method: "こんにちは、postさん"}))
+  .post("/hook/", (req, res) => res.json({ test: "hook" }))// 追加
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
