e7aa2b16bfed5953051284a847068cbbce877772
yuu551 <65840312+yuu551@users.noreply.github.com>
2020-09-12 23:04:14 +0900

    test

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const config = {
   channelAccessToken: process.env.ACCESS_TOKEN,
   channelSecret: process.env.SECRET_KEY
 };
+const client = new line.Client(config); // 追加
 
 
 express()
@@ -20,6 +21,22 @@ express()
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
   function lineBot(req, res) {
-    res.json({ test: "hook" })
-    console.log("pass"); // 追加
+    res.status(200).end(); // 変更
+    const events = req.body.events;
+    const promises = [];
+    for (let i = 0, l = events.length; i < l; i++) {
+      const ev = events[i];
+      promises.push(
+        echoman(ev)
+      );
+    }
+    Promise.all(promises).then(console.log("pass"));
+  }
+
+  async function echoman(ev) {
+    const pro =  await client.getProfile(ev.source.userId);
+    return client.replyMessage(ev.replyToken, {
+      type: "text",
+      text: `${pro.displayName}さん、今「${ev.message.text}」って言いました？`
+    })
   }
