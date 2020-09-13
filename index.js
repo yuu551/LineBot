b2487b1f772ce6da56b41c132a487fc6c41b827d
yuu551 <65840312+yuu551@users.noreply.github.com>
2020-09-13 23:17:17 +0900

    tets

--- index.js
+++ index.js
@@ -92,7 +92,114 @@ async function handleEvent(event) {
     
   }
   // ヒットしたインドカレー店の住所をLINE botに返す
-  return client.replymessage(event.replyToken, msg12)
+  return client.replymessage(event.replyToken, [{
+    type: 'text',
+    text: '検索結果付近の店舗はこちらです！'
+  },
+  {
+    "type": "flex",
+  "altText": "#",
+  "contents":{
+    "type": "bubble",
+  "hero": {
+    "type": "image",
+    "url": curry_pic,
+    "size": "full",
+    "aspectRatio": "20:13",
+    "aspectMode": "cover",
+    "action": {
+      "type": "uri",
+      "uri": curry_url
+    }
+  },
+  "body": {
+    "type": "box",
+    "layout": "vertical",
+    "contents": [
+      {
+        "type": "text",
+        "text": shop_name,
+        "weight": "bold",
+        "size": "xl"
+      },
+      {
+        "type": "box",
+        "layout": "vertical",
+        "margin": "lg",
+        "spacing": "sm",
+        "contents": [
+          {
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Place",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": address,
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
+          },
+          {
+            "type": "box",
+            "layout": "baseline",
+            "spacing": "sm",
+            "contents": [
+              {
+                "type": "text",
+                "text": "Time",
+                "color": "#aaaaaa",
+                "size": "sm",
+                "flex": 1
+              },
+              {
+                "type": "text",
+                "text": opentime,
+                "wrap": true,
+                "color": "#666666",
+                "size": "sm",
+                "flex": 5
+              }
+            ]
+          }
+        ]
+      }
+    ]
+  },
+  "footer": {
+    "type": "box",
+    "layout": "vertical",
+    "spacing": "sm",
+    "contents": [
+      {
+        "type": "button",
+        "style": "link",
+        "height": "sm",
+        "action": {
+          "type": "uri",
+          "label": "WEBSITE",
+          "uri": curry_url
+        }
+      },
+      {
+        "type": "spacer",
+        "size": "sm"
+      }
+    ],
+    "flex": 0
+  }
+}
+  }]);
 }
 
 if(event.type == 'message'){
