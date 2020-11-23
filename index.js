0283d0e8d254d69930b35e4a59d2aebd9912aa5f
Yudai Jinno <nakou36@gmail.com>
2020-11-24 00:52:39 +0900

    修正

--- index.js
+++ index.js
@@ -194,7 +194,8 @@ if(event.message.text == 'お気に入りを表示'){
     console.log(address)
     console.log(opentime)
     console.log(shopid)
-    .then( () => {
+  }
+  await test().then( () => {
   msg = curmsg.replymessage(curry_pic,curry_url,shop_name,address,opentime,shopid);
   // ヒットしたインドカレー店の住所をLINE botに返す
   return client.replyMessage(event.replyToken,[{
@@ -205,10 +206,6 @@ if(event.message.text == 'お気に入りを表示'){
     
   
   });
-}
-console.log("確認君");
-console.log(test)
-return test;
 
 }
 
