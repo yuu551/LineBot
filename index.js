cb1ea8998c0585efc7f3dc88a384d5dba515210c
Yudai Jinno <nakou36@gmail.com>
2020-11-24 00:24:54 +0900

    修正

--- index.js
+++ index.js
@@ -188,12 +188,6 @@ if(event.message.text == 'お気に入りを表示'){
             
      }
     }
-    console.log(curry_pic);
-    console.log(curry_url)
-    console.log(shop_name)
-    console.log(address)
-    console.log(opentime)
-    console.log(shopid)
   }).call().then( () => {
   msg = curmsg.replymessage(curry_pic,curry_url,shop_name,address,opentime,shopid);
 
