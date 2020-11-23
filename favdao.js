1a3883305edbc10dd052434fa822ad4a720158be
Yudai Jinno <nakou36@gmail.com>
2020-11-23 17:34:05 +0900

    修正

--- favdao.js
+++ favdao.js
@@ -1,23 +1,23 @@
 const Airtable = require('airtable-node');
 
+const getWorks = () => {
+  return new Promise((resolve, reject) => {
+    const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
+    const APP_ID = process.env.APP_ID;
+    console.log(AIRTABLE_API_KEY)
+    console.log(APP_ID)
+    const worksTable = new Airtable({ apiKey: AIRTABLE_API_KEY })
+      .base(APP_ID)
+      .table('FavTable1')
 
-exports.Test = function(){
-
-        return new Promise((resolve, reject) => {
-          const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
-          const APP_ID = process.env.APP_ID;
-          console.log(AIRTABLE_API_KEY)
-          console.log(APP_ID)
-          const worksTable = new Airtable({ apiKey: AIRTABLE_API_KEY })
-            .base(APP_ID)
-            .table('FavTable1')
-          console.log(worksTable);
-          return worksTable
-            .list({
-              view: 'Grid view' // 作成したAppで使用しているViewを指定
-            })
-            .then(response => {
-              resolve(response)
-            })
-        })
+    return worksTable
+      .list({
+        view: 'Grid view' // 作成したAppで使用しているViewを指定
+      })
+      .then(response => {
+        resolve(response)
+      })
+  })
 }
+
+exports.Test = getWorks;
