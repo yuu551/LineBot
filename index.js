af7872b4bc01d5151d983a492880323a1f0c01d8
Yudai Jinno <nakou36@gmail.com>
2020-11-23 17:12:15 +0900

    修正

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const express = require('express');
 const line = require('@line/bot-sdk');
 const msgword =require('./messages');
 const curmsg =require('./cursel');
-const dao = require('./favdao');
 
 const PORT = process.env.PORT || 3000;
 
@@ -40,7 +39,6 @@ async function handleEvent(event) {
       var msgs = [];
       console.log(event);
       console.log(event.source.userId);
-      console.log(dao.Test);
   
 
   ///メニューから位置情報で検索ボタンを押したとき
