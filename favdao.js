6b9b639b291f98788f0fcb513459b5bb260ae09a
Yudai Jinno <nakou36@gmail.com>
2020-11-23 18:27:35 +0900

    修正

--- favdao.js
+++ favdao.js
@@ -1,6 +1,7 @@
 const Airtable = require('airtable-node');
 
 const getWorks = () => {
+  return new Promise((resolve, reject) => {
     const AIRTABLE_API_KEY = process.env.AIRTABLE_API_KEY;
     const APP_ID = process.env.APP_ID;
     console.log(AIRTABLE_API_KEY)
@@ -8,7 +9,13 @@ const getWorks = () => {
     const worksTable = new Airtable({ apiKey: AIRTABLE_API_KEY })
       .base(APP_ID)
       .table('FavTable1')
-    return worksTable.list()
+    
+    return worksTable
+      .list()
+      .then(response => {
+        resolve(response)
+      })
+  })
 }
 
 exports.Test = getWorks;
